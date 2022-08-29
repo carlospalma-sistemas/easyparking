3f84287e1eba0ce09dc142b653b6ef712f54f56d
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-08-29 18:46:00 -0500

    Initial commit

---
+++ src/main/java/com/easyparking/EasyparkingApplication.java
@@ -0,0 +1,13 @@
+package com.easyparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyparking/EasyparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
